9fba451ba250d85ae2ce01ac3246e50e0ba628d4
Yaw Boakye-Owusu <yawboakye-owusu@yaws-imac.home>
2019-11-25 07:03:30 +0000

    material ui added

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-container">
+        <p className="app-results">0</p>
+        <div className="app-buttons"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
